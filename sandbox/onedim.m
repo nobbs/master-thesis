59cd2e4e49d334c8d8a9001cd87d85a217ab3eee
Alexej Disterhoft <alexej@disterhoft.de>
2015-03-04 10:38:42 +0100

    onedim: \omega als Reihe mit Parametern.
    \omega in Sinusreihe entwickelt mit Parametern. Diese werden (noch) zufällig
    gewählt. Es wird die Lösung und die Sensitivitäten der vorkommenden Parameter
    bestimmt.

--- sandbox/onedim.m
+++ sandbox/onedim.m
@@ -1,102 +1,141 @@
 % Numerische Lösung mittels Linienmethode / Finite Differenzen für den
 % eindimensionalen Fall mit reellwertigem Parameter \omega.
 function [output] = onedim(plot_all)
-    if nargin == 0
-        plot_all = 0;
-    end
+if nargin == 0
+    plot_all = 0;
+end
 
-    % Zeit und Ortsintervalle
-    t_span  = [0, 1];
-    x_span  = [0, 1];
-    
-    % Ortsdiskretisierung
-    x_steps = 50;
-    x_grid  = linspace(x_span(1), x_span(2), x_steps);
-    
-    X       = x_grid(2:end-1);
-    x_h     = x_grid(2) - x_grid(1);
-    x_N     = x_steps - 2;
+% Zeit und Ortsintervalle
+t_span  = [0, 1];
+x_span  = [0, 1];
 
-    % Konstanten
-    sigma = 1;
-    
-    % Anfangsbedingung
+% Ortsdiskretisierung
+x_steps = 25;
+x_grid  = linspace(x_span(1), x_span(2), x_steps);
+
+X       = x_grid(2:end-1);
+x_h     = x_grid(2) - x_grid(1);
+x_N     = x_steps - 2;
+
+% Konstanten
+sigma = 1;
+
+% Anfangsbedingung
     function u = u_0(x)
-        % u = x*sin(pi*x);
-        % u = exp(-5 * (x - 0.5)^2) - exp(-5/4);
-        u = exp(-5 * (x - 0.5).^2) - exp(-5/4) - 0.5 * (exp(-30 * (x - 0.5).^2) - exp(-30/4));
+        u = x.*sin(pi*x);
+%         u = exp(-5 * (x - 0.5)^2) - exp(-5/4);
+%         u = exp(-5 * (x - 0.5).^2) - exp(-5/4) - 0.5 * (exp(-30 * (x - 0.5).^2) - exp(-30/4));
     end
-    
-    % Reaktionsterm
-    function y = w(x)
-        y = 0;
-    end
-    
-    % Quellterm
+
+% Reaktionsterm
+N_Sigmas = 5;
+factor = 20;
+Sigmas = factor * (rand(N_Sigmas, 1) - 0.5);
+w = generate_w(N_Sigmas, Sigmas);
+%     plot(x_grid, w(x_grid));
+%     return;
+
+% Quellterm
     function y = g(t, x)
         y = 0;
     end
-    
-    % "exakte" Lösung
+
+% "exakte" Lösung
     function fun = generateSolution(u_0_fun, N)
+        %         w = @(x) 0;
         fun = @(t, x) 0;
         for i = 1:N
             coeff = integral(@(x) 2 * u_0_fun(x) .* sin(i * pi *x), 0, 1);
             fun   = @(t, x) fun(t, x) + coeff * sin(i * pi * x) .* exp(-(i^2 * pi^2 * sigma + w(x)) .* t);
         end
     end
-    
-    % Erzeuge Massematrix
-    tmp = ones(x_N, 1);
-    M   = (1 / x_h)^2 * spdiags([tmp, -2*tmp, tmp], -1:1, x_N, x_N);
 
-    tmp = arrayfun(@(x) w(x), x_grid(2:end-1))';
-    W   = spdiags(tmp, [0], x_N, x_N);
-    
-    % ODE-System
+% Erzeuge Massematrix
+tmp = ones(x_N, 1);
+M   = (1 / x_h)^2 * spdiags([tmp, -2*tmp, tmp], -1:1, x_N, x_N);
+
+
+W = zeros(x_N, x_N);
+for j = 1:x_N
+    for k = 1:N_Sigmas
+        if k == 1
+            W(j, j) = Sigmas(k);
+        else
+            W(j, j) = W(j, j) + Sigmas(k) * sqrt(2 / (1 + k^2 * pi^2)) * sin(pi * k * (j * x_h));
+        end
+    end
+end
+
+% ODE-System
     function dy = ode(t, y)
-        dy  = zeros(3 * (x_N), 1);
-        Mat = [
-            sigma * M - W, zeros(x_N), zeros(x_N);
-            - eye(x_N), sigma * M - W,  zeros(x_N);
-            zeros(x_N), - 2 * eye(x_N), sigma * M - W;
-            ];
-        dy  = Mat * y + [arrayfun(@(x) g(t, x), x_grid(2:end-1))'; zeros(x_N, 1); zeros(x_N, 1)];
+        Mat = zeros((1 + N_Sigmas) * (x_N));
+        Mat(1:x_N, 1:x_N) = sigma * M - W;
+        
+        for j = 1:N_Sigmas
+            rows = (j * x_N +1):((j+1) * x_N);
+            colsA = 1:x_N;
+            colsB = (j * x_N +1):((j+1) * x_N);
+            for k = 1:x_N
+                if j == 1
+                    Mat(rows(k), colsA(k)) = -1;
+                else
+                    Mat(rows(k), colsA(k)) = -sqrt(2 / (1 + j^2 * pi^2)) * sin(pi * j * (k * x_h));
+                end
+            end
+            Mat(rows, colsB) = sigma * M - W;
+        end
+        
+        dy  = Mat * y;
+        dy(colsA) = dy(colsA) + arrayfun(@(x) g(t, x), x_grid(2:end-1))';
     end
 
-    % Anfangswerte
-    y_0 = [arrayfun(@(x) u_0(x), x_grid(2:end-1))'; zeros(x_N, 1); zeros(x_N, 1)];
-    
-    % ODE-Solver loslassen
-    [T, Y] = ode23s(@(t, y) ode(t, y), t_span, y_0);
-    
-    % Lösung aufteilen in eigentliche ODE und Sensitivity Analysis Teil
-    [T_mesh, X_mesh] = meshgrid(T, X);
-    u   = Y(:, 1:(x_N));
-    uw  = Y(:, (x_N + 1):(2*x_N));
-    uw2 = Y(:, (2*x_N + 1):(3*x_N));
+% Anfangswerte
+y_0 = zeros((1 + N_Sigmas) * x_N, 1);
+y_0(1:x_N) = arrayfun(@(x) u_0(x), x_grid(2:end-1));
 
-    % "exakte" Lösung generieren
-    exU   = generateSolution(@(x) u_0(x), 16);
-    exUw  = @(t, x) - t .* exU(t, x);
-    exUw2 = @(t, x) t.^2 .* exU(t, x);
+% ODE-Solver loslassen
+[T, Y] = ode23s(@(t, y) ode(t, y), t_span, y_0);
 
-    % Plots
-    figure(1);
-    mesh(T_mesh, X_mesh, abs(u' - exU(T_mesh, X_mesh)));
-    title('Fehler für u');
+% Lösung aufteilen in eigentliche ODE und Sensitivity Analysis Teil
+[T_mesh, X_mesh] = meshgrid(T, X);
+u   =  Y(:, 1:(x_N));
+
+figure(1);
+mesh(T_mesh, X_mesh, u');
+title('Lösung u');
+xlabel('t');
+ylabel('x');
+
+for j = 1:N_Sigmas
+    u_sigma  = Y(:, (j*x_N + 1):((j+1)*x_N));
+    figure(j + 1);
+    mesh(T_mesh, X_mesh, u_sigma');
+    title('Sensitivity u_{\sigma_j}');
     xlabel('t');
     ylabel('x');
-    
-    figure(2);
-    mesh(T_mesh, X_mesh, abs(uw' - exUw(T_mesh, X_mesh)));
-    title('Fehler für u_\omega');
-    xlabel('t');
-    ylabel('x');
+end
 
-    figure(3);
-    mesh(T_mesh, X_mesh, abs(uw2' - exUw2(T_mesh, X_mesh)));
-    title('Fehler für u_{\omega\omega}');
-    xlabel('t');
-    ylabel('x');
+% "exakte" Lösung generieren
+% exU   = generateSolution(@(x) u_0(x), 16);
+% exUw  = @(t, x) - t .* exU(t, x);
+% exUw2 = @(t, x) t.^2 .* exU(t, x);
+
+% Plots
+% figure(1);
+% mesh(T_mesh, X_mesh, abs(u' - exU(T_mesh, X_mesh)));
+% title('Fehler für u');
+% xlabel('t');
+% ylabel('x');
+%
+% figure(2);
+% mesh(T_mesh, X_mesh, abs(uw' - exUw(T_mesh, X_mesh)));
+% title('Fehler für u_\omega');
+% xlabel('t');
+% ylabel('x');
+%
+% figure(3);
+% mesh(T_mesh, X_mesh, abs(uw2' - exUw2(T_mesh, X_mesh)));
+% title('Fehler für u_{\omega\omega}');
+% xlabel('t');
+% ylabel('x');
 end
