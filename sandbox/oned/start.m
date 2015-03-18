be5786bd6f49f06840dee0509c61be32b29a5238
Alexej Disterhoft <alexej@disterhoft.de>
2015-03-18 22:59:09 +0100

    FE: Unnötiges entfernt

--- sandbox/oned/start.m
+++ sandbox/oned/start.m
@@ -2,13 +2,13 @@ tspan = [0 1];
 xspan = [0 1];
 
 % Anzahl Basisfunktionen für X
-num_X_j = 5;
-num_X_k = 5;
+num_X_j = 15;
+num_X_k = 15;
 
 % Anzahl Basisfunktionen für Y
-num_Y_l = 5;
-num_Y_m = 5;
-num_Y_n = 5;
+num_Y_l = 15;
+num_Y_m = 15;
+num_Y_n = 15;
 
 % Dimension von X und Y
 dim_X = num_X_j * num_X_k;
@@ -22,7 +22,7 @@ legendre_polys_derivative = shifted_legendre_polynomials_derivative(max(num_X_k,
 % Y_h = \span{ ( \sin(l * \pi * x) * P_m, \sin(n * pi * x) ) : \nu = 1..num_m, \mu = 1..num_q, j = 1..num_m }
 
 % Steifigkeitsmatrix aufsetzen
-B = zeros(dim_Y, dim_X);
+B = sparse(dim_Y, dim_X);
 
 % \omega und Anfangswert
 w = @(x) 0 + 0 .* x;
@@ -32,31 +32,16 @@ for j = 1:num_X_j
     for k = 1:num_X_k
         for l = 1:num_Y_l
             for m = 1:num_Y_m
-                % tmp1 = integral2(@(x, t) sinus_funcs{j}(x) .* ...
-                %     legendre_polys_derivative{k}(t) .* ...
-                %     sinus_funcs{l}(x) .* ...
-                %     legendre_polys{m}(t), xspan(1), xspan(2), tspan(1), tspan(2));
-
                 tmp1 = 0;
                 if j == l
                     tmp1 = quadgk(@(t) legendre_polys_derivative{k}(t) .* ...
                             legendre_polys{m}(t), tspan(1), tspan(2)) / 2;
                 end;
 
-                % tmp2 = integral2(@(x, t) sinus_funcs_derivative{j}(x) .* ...
-                %     legendre_polys{k}(t) .* ...
-                %     sinus_funcs_derivative{l}(x) .* ...
-                %     legendre_polys{m}(t), xspan(1), xspan(2), tspan(1), tspan(2));
-
                 tmp2 = 0;
                 if j == l && k == m
                     tmp2 = ((2 * pi * j)^2 / (2 * (k - 1) + 1)) / 2;
                 end;
-
-                % tmp3 = integral2(@(x, t) w(x) .* sinus_funcs{j}(x) .* ...
-                %     legendre_polys{k}(t) .* ...
-                %     sinus_funcs{l}(x) .* ...
-                %     legendre_polys{m}(t), xspan(1), xspan(2), tspan(1), tspan(2));
 
                 tmp3 = 0;
                 if k == m
@@ -66,10 +51,6 @@ for j = 1:num_X_j
                 for n = 1:num_Y_n
                     y_pos = (j - 1) * num_X_k + k;
                     x_pos = (l - 1) * (num_Y_m * num_Y_n) + (m - 1) * num_Y_n + n;
-
-                    % tmp4 = integral(@(x) sinus_funcs{j}(x) .* ...
-                    %     legendre_polys{k}(0) .* ...
-                    %     sinus_funcs{n}(x), xspan(1), xspan(2));
 
                     tmp4 = 0;
                     if (j == n)
@@ -84,7 +65,7 @@ for j = 1:num_X_j
 end
 
 % Lastvektor
-F = zeros(dim_Y, 1);
+F = sparse(dim_Y, 1);
 for l = 1:num_Y_l
     for m = 1:num_Y_m
         for n = 1:num_Y_n
