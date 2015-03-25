fc450a711bc8451bc35288e83be30f16a99222bc
Alexej Disterhoft <alexej@disterhoft.de>
2015-03-25 14:38:26 +0100

    Bestimmung der Stetigkeitskonstante, neuer Versuch

--- sandbox/continuity_bound/continuity_bound.m
+++ sandbox/continuity_bound/continuity_bound.m
@@ -1,11 +1,11 @@
-% Berechnet die Stetigkeits-Konstante für $A_j$ mit 
+% Berechnet die Stetigkeits-Konstante für $A_j$ mit
 % $\phi_j = \frac{\sin(\pi * j * x)}{(j*\pi)^{1 + \epsilon})}.
 
 % Anzahl Sinuskoeffizienten (insgesamt 2 * N + 1)
-N = 25;
+N = 10;
 
 % Anzahl Schleifendurchläufe
-M = 2500;
+M = 1000;
 
 % Zu testendes $\phi_j$
 l       = 20;
@@ -14,28 +14,32 @@ K       = 5;
 phi     = @(x) K * sin(l*pi*x) / (l*pi)^(1+epsilon);
 
 xspan = [0, 1];
-values = zeros(M, 1);
+values = zeros(N * M, 1);
 
-for k = 1:M
-    if mod(k, 50) == 0
-        k
+for l = 1:N
+    l
+    for k = 1:M
+        if mod(k, 50) == 0
+            k
+        end
+        
+        % zufällig Sinuskoeffizienten generieren
+        ucoeffs = 2 * (rand(l, 1) - 0.5);
+        vcoeffs = 2 * (rand(l, 1) - 0.5);
+        
+        % Skalarprodukt, Normen und Schranke bestimmen
+        unorm = h1_norm_sine(ucoeffs);
+        vnorm = h1_norm_sine(vcoeffs);
+        
+        % Funktionen aus Sinuskoeffizienten erzeugen
+        u  = @(x) generate_sine_series(x, ucoeffs / unorm);
+        v  = @(x) generate_sine_series(x, vcoeffs / vnorm);
+        
+        values((l - 1) * M + k) = abs(l2_skp(@(x) phi(x) .* u(x), v, xspan));
     end
-    
-    % zufällig Sinuskoeffizienten generieren
-    ucoeffs = rand(N, 1);
-    vcoeffs = rand(N, 1);
-    
-    % Skalarprodukt, Normen und Schranke bestimmen
-    unorm = h1_norm_sine(ucoeffs);
-    vnorm = h1_norm_sine(vcoeffs);
-
-    % Funktionen aus Sinuskoeffizienten erzeugen
-    u  = @(x) generate_sine_series(x, ucoeffs / unorm);
-    v  = @(x) generate_sine_series(x, vcoeffs / vnorm);
-    
-    values(k) = abs(l2_skp(@(x) phi(x) .* u(x), v, xspan));
 end
 
 figure()
 max(values)
-scatter(1:M, values);
+% scatter(1:length(values), values);
+
